57777b9fa5a9cbe89db88d0483a86424aa2403c4
Edgar Cordero <edgar.cordero2890@alumnos.udg.mx>
2020-10-22 15:24:18 -0500

    Donde se ejecuta el programa

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "laboratory.h"
+
+
+using namespace std;
+
+int main()
+{
+    Laboratory l;
+    l.recuperar();
+    l.mostrar();
+    l.respaldarTabla();
+
+//    Computer c01 = Computer ("Tu computadora","Windows 10","CORE i5",64);
+
+
+//    Computer c02;
+
+//    c02.setName("Mi Computadora");
+//    c02.setSystem("Lynux");
+//    c02.setCpu("CORE i5");
+//    c02.setRam(32);
+
+//    Laboratory lb;
+    //lb.addComputer(c01);
+    //lb.addComputer(c02);
+
+//    Computer c03;
+//    cin >> c03;
+    //lb.addComputer(c03);
+//    lb << c01 << c02 << c03;
+
+//    lb.mostrar();
+    //lb.respaldarTabla();
+    //lb.respaldar();
+
+
+}
